52c958d1a65f64199246d78166fd9697c2922b2e
scordoni <54602744+scordoni@users.noreply.github.com>
2021-10-30 16:53:06 -0400

    redid infections and set up testing

--- Semester Project/SimulationCordoni.java
+++ Semester Project/SimulationCordoni.java
@@ -32,24 +32,44 @@ public class SimulationCordoni {
         //start with population being 1000
         int[] population = new int[Integer.parseInt(args[0])];
 
+        double populationSize = Double.parseDouble(args[0]);
+
         int groupNumber = 0;
 
         int groupSize = 8;
         
+        double infectionRate = 0.02;
+
+        double numberInfected = 0.0;
+
+        int index = 0;
+
+        int numberOfTests = 0;
+
+        int positiveCase = 0;
+
+        int negativeCase = 0;
 
         //we are going to let 1 be infected and let 0 be healthy
         //This method is going to randomly infect the population
 
-        for (int i = 0; i < population.length; i++){
+        numberInfected = populationSize * infectionRate;
 
-            population[getRandomIndex(population)] = 1;
+        int intNumberInfected = (int) numberInfected;
+        
+        System.out.println(intNumberInfected);
+
+        for (int i = 0; i < intNumberInfected ; i++){
+
+            population[getRandomInfection(population)] = 1;
 
         }//for
+        
 
         //print out the population
         for (int i = 0; i < population.length; i++){
 
-           // System.out.println(population[i]);
+           //System.out.println(population[i]);
             
         }//for
 
@@ -57,39 +77,75 @@ public class SimulationCordoni {
         //want to make a 2 dimensional array so that we can go through each group of 8
         groupNumber = (Integer.parseInt(args[0])) / groupSize ;
     
+        //here we are going to make a copy of our group array to seach through so that 
+        //we still have an original 
         int[][] grouparray = new int [groupNumber][groupSize];
 
+        int[][] groupArrayCopy = grouparray.clone();
+
+       
         //go through the group array and input the population
         //then in each index of the group make an array the size of group size
-        for (int i = 0; i < groupNumber - 1; i++){
+        for (int i = 0; i < groupNumber; i++){
 
-            for ( int j = 0; j < groupSize - 1; j++){
+            for ( int j = 0; j < groupSize; j++){
 
-                grouparray[i][j] = population[i];
-
+                grouparray[i][j] = population[index];
+                index++;
             }//for j
         }//for i
 
         //print out the group array to see each group of 8
-        //System.out.println(Arrays.deepToString(grouparray));
+        System.out.println(Arrays.deepToString(grouparray));
                 
-        
+
         //now we test for infections!
+        //testing 125 groups of 8
+        for (int i = 0; i < groupNumber; i++){
 
+            if(groupArrayCopy[i] == 1){
+                //then we divide
+                
+              if((groupArrayCopy[i][j] == 1) && (groupArrayCopy[i][j] == 0)){
 
+                //test all members of infected group
+              }//if
+
+              //else both groups are infected and all must be tested
+              else{
+
+                    for ( int j = 0; j < groupSize; j++){
         
-        //then go through each group, 
-        //then make copy, if 1 then whole postiive
-        //then make 2 copy and go through 1-4 and 4-8
-        //then make 3 copy and go through individually 
+                        numberOfTests++;
+
+                        if (groupArrayCopy[i][j] == 1){
+                            positiveCase++;
+                        }//if
+
+                        else{
+                            negativeCase++;
+                        }//else
+
+                    }//for j
+                
+              }//else
+
+            }//if
+
+            //else there was no infection found
+            else{
+                numberOfTests++;
+            }//else
 
 
+        }//for
+        
 
        
     }//main
 
-    //This method gets the random indexes to infect the population
-    public static int getRandomIndex(int[] populationArray)
+    //This method randomly infects the population
+    public static int getRandomInfection(int[] populationArray)
     {
         int randomIndex = 0;
 
@@ -101,9 +157,8 @@ public class SimulationCordoni {
 
         return randomIndex;
         
-    }//getRandomIndex
+    }//getRandomInfection
 
 
 
-}//simulation cordoni
-
+}//semester cordoni
